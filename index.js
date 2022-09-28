11aca4ded36aff340a8a0a9313043d6fd6f95394
theocita <theocitaallen@gmail.com>
2022-09-27 20:34:07 -0400

    completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+     customerName =  customerName.toUpperCase();
+};
+
+function setBestCustomer(){
+   bestCustomer = 'not bob';
+};
+bestCustomer;
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+};
+
+
+
+function changeLeastFavoriteCustomer(){
+    const leastFavoriteCustomer = 'jeff';
+     leastFavoriteCustomer = 'barry';
+};
